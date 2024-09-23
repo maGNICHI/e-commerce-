d1146d95d672c37cd3aa143706df321c8d653fd7
maGNICHI <manar.gnichi@esprit.tn>
2024-09-24 00:28:57 +0100

    autre modification

--- achat/achatenligne/urls.py
+++ achat/achatenligne/urls.py
@@ -12,14 +12,16 @@ urlpatterns = [
     path('dashboard/produits/ajouter/', views.ajouter_produit, name='ajouter_produit'),
     path('dashboard/produits/edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('dashboard/produits/delete/<int:product_id>/', views.delete_product, name='delete_product'),
-    path('dashboard/post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('dashboard/post/', views.affiche_posts, name='affiche_posts'),
     path('dashboard/post/ajouter/', views.ajouter_post, name='ajouter_post'),
+    path('dashboard/post/edit/<int:id>/', views.edit_post, name='edit_post'),
+    path('dashboard/post/delete/<int:id>/', views.delete_post, name='delete_post'),
     #template
     path('produit/', views.afficher_produit, name='afficher_produit'),
     path('', views.index, name='index'),
     path('categorie/', views.afficher_categorie, name='afficher_categorie'),
    
-     path('post/', views.affiche_posts, name='affiche_posts'),
+    path('post/', views.affiche_posts, name='affiche_posts'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('post/ajouter/', views.ajouter_post, name='ajouter_post'),
     path('post/<int:post_id>/modifier/', views.edit_post, name='edit_post'),
